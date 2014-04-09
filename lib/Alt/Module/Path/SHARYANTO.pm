e1099de416c0c601c09704a8b2813d0862422f4b
Steven Haryanto <stevenharyanto@gmail.com>
2014-04-09 14:04:13 +0000

    Import of SHARYANTO/Alt-Module-Path-SHARYANTO-0.01 from CPAN.
    gitpan-cpan-distribution: Alt-Module-Path-SHARYANTO
    gitpan-cpan-version:      0.01
    gitpan-cpan-path:         SHARYANTO/Alt-Module-Path-SHARYANTO-0.01.tar.gz
    gitpan-cpan-author:       SHARYANTO
    gitpan-cpan-maturity:     released

---
+++ lib/Alt/Module/Path/SHARYANTO.pm
@@ -0,0 +1,56 @@
+package Alt::Module::Path::SHARYANTO;
+use base qw(Alt::Base);
+
+our $VERSION = '0.01'; # VERSION
+
+1;
+# ABSTRACT: SHARYANTO's alternate implementation of Module::Path
+
+__END__
+
+=pod
+
+=encoding UTF-8
+
+=head1 NAME
+
+Alt::Module::Path::SHARYANTO - SHARYANTO's alternate implementation of Module::Path
+
+=head1 VERSION
+
+version 0.01
+
+=head1 DESCRIPTION
+
+=head1 SEE ALSO
+
+L<Alt>
+
+=head1 HOMEPAGE
+
+Please visit the project's homepage at L<https://metacpan.org/release/Alt-Module-Path-SHARYANTO>.
+
+=head1 SOURCE
+
+Source repository is at L<https://github.com/sharyanto/perl-Alt-Module-Path-SHARYANTO>.
+
+=head1 BUGS
+
+Please report any bugs or feature requests on the bugtracker website L<https://rt.cpan.org/Public/Dist/Display.html?Name=Alt-Module-Path-SHARYANTO>
+
+When submitting a bug or request, please include a test-file or a
+patch to an existing test-file that illustrates the bug or desired
+feature.
+
+=head1 AUTHOR
+
+Steven Haryanto <stevenharyanto@gmail.com>
+
+=head1 COPYRIGHT AND LICENSE
+
+This software is copyright (c) 2014 by Steven Haryanto.
+
+This is free software; you can redistribute it and/or modify it under
+the same terms as the Perl 5 programming language system itself.
+
+=cut
